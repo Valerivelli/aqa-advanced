527009690b0d0ee5175391717c22fd6d1e39bb80
valeriiakyrylova <artuuutvelli@gmail.com>
2025-09-16 16:03:44 +0300

    HW4 updates in task1 and task4.3

--- jsBasics/task4.3.js
+++ jsBasics/task4.3.js
@@ -1,5 +1,5 @@
 const radius = 5;
 const height = 14;
-let area = (Math.PI * (radius ** 2) * height).toFixed(2);
+let cylinderVolume = (Math.PI * (radius ** 2) * height).toFixed(2);
 
-console.log(area);
+console.log(cylinderVolume);
