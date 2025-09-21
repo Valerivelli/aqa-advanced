54b2b7be74cbebf81510245f0a25ff527326e55e
valeriiakyrylova <artuuutvelli@gmail.com>
2025-09-21 21:25:37 +0300

    HW6 (added 3 tasks)

---
+++ jsFunctions/task1.js
@@ -0,0 +1,15 @@
+//function declaration
+function calculateRectangleArea(width, height) {
+    return width * height;
+}
+console.log('Function declaration: ', calculateRectangleArea(5, 10));
+
+//function expression
+const calculateRectangleAreaExpr = function (width, height) {
+    return width * height;
+};
+console.log("Function Expression:", calculateRectangleAreaExpr(5, 10));
+
+//arrow function
+const calculateRectangleAreaArrow = (width, height) => width * height;
+console.log("Arrow Function:", calculateRectangleAreaArrow(5, 10));
