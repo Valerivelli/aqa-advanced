720952d1e5ddb82d2ede48aaeda2bd2ea88287c8
valeriiakyrylova <artuuutvelli@gmail.com>
2025-09-21 14:50:04 +0300

    HW5 (added 3 tasks)

---
+++ jsConditionsAndLoops/task3.js
@@ -0,0 +1,13 @@
+const number = 9;
+
+console.log("Таблиця множення (цикл for):");
+for (let i = 1; i <= 10; i++) {
+    console.log(number + ' x ' + i + " = " + (number * i));
+}
+
+console.log("Таблиця множення (цикл while):");
+let j = 1;
+while (j <= 10) {
+    console.log(number + ' x ' + j + " = " + (number * j));
+    j++;
+}
