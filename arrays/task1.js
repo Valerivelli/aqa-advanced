c42977e52da276cf4f8222b6dd981c8694791a4c
valeriiakyrylova <artuuutvelli@gmail.com>
2025-10-03 16:30:53 +0300

    HW8 (added 6 tasks)

---
+++ arrays/task1.js
@@ -0,0 +1,22 @@
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8]
+let positiveCount = 0;
+let negativeCount = 0;
+let zeroCount = 0;
+
+
+for (let i = 0; i < numbers.length; i++) {
+    validateNumber(numbers[i]);
+}
+console.log("Count of positive Numbers: ", positiveCount);
+console.log("Count of negative Numbers: ", negativeCount);
+console.log("Count of zero Numbers: ", zeroCount);
+
+function validateNumber(number) {
+    if (number < 0) {
+        negativeCount++;
+    } else if (number === 0) {
+        zeroCount++;
+    } else if (number > 0) {
+        positiveCount++;
+    }
+}
